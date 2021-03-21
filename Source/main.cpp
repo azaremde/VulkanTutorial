2d66b699ae42baef7e5c6c2268bc06107d3c8468
azaremde <azaremde@gmail.com>
2021-03-21 09:59:20 +0100

    Triangle > Drawing > Framebuffers

--- Source/main.cpp
+++ Source/main.cpp
@@ -61,6 +61,8 @@ private:
 	VkExtent2D swapChainExtent;
 
 	Avec<VkImageView> swapChainImageViews;
+
+	Avec<VkFramebuffer> swapChainFramebuffers;
 	// -------------------------
 
 	// ------- Pipeline --------
@@ -109,6 +111,33 @@ private:
 		createImageViews();
 		createRenderPass();
 		createGraphicsPipeline();
+		createFramebuffers();
+	}
+
+	void createFramebuffers()
+	{
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+
+		for (size_t i = 0; i < swapChainImageViews.size(); i++)
+		{
+			VkImageView attachments[] = {
+				swapChainImageViews[i]
+			};
+
+			VkFramebufferCreateInfo framebufferInfo{};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+
+			if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+			{
+				throw std::runtime_error("Failed to create framebuffer");
+			}
+		}
 	}
 
 	static Avec<char> readFile(const Astr& filename)
@@ -823,6 +852,11 @@ private:
 
 	void cleanUp()
 	{
+		for (auto& framebuffer : swapChainFramebuffers)
+		{
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		}
+
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
