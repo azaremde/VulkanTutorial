5a1412b6b421ea47c1e09fee216ba5971117acb4
azaremde <azaremde@gmail.com>
2021-04-01 21:29:50 +0200

    Update Pipeline.cxx

--- Source/AstrumVK/Pipeline/Pipeline.cxx
+++ Source/AstrumVK/Pipeline/Pipeline.cxx
@@ -33,10 +33,12 @@ void Pipeline::createDescriptorSetLayout()
     uboLayoutBinding.pImmutableSamplers = nullptr;
     uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
 
+    std::vector<VkDescriptorSetLayoutBinding> bindings = { uboLayoutBinding };
+
     VkDescriptorSetLayoutCreateInfo layoutInfo{};
     layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-    layoutInfo.bindingCount = 1;
-    layoutInfo.pBindings = &uboLayoutBinding;
+    layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
+    layoutInfo.pBindings = bindings.data();
     
     VK_CHECK(
         vkCreateDescriptorSetLayout(gpu.getDevice(), &layoutInfo, nullptr, &descriptorSetLayout),
